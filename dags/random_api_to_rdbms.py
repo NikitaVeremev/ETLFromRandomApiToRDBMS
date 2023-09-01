d00499bdbe866b1b3eba857dc0f45f8af784965a
NikitaVeremev <veremev.na@phsytech.edu>
2023-09-01 12:30:14 +0300

    add with to dag

--- dags/random_api_to_rdbms.py
+++ dags/random_api_to_rdbms.py
@@ -6,11 +6,6 @@ from scripts.api_to_postgres import ApiToPostgresOperator
 
 dag_id = 'from_api_to_rdbms'
 
-dag = DAG(dag_id,
-          schedule_interval='0 */12 * * *',
-          start_date=days_ago(2),
-          catchup=False)
-
 target_table_ddl_ch = '''
         CREATE TABLE IF NOT EXISTS cannabis_api_summary (
           id UInt64,
@@ -29,20 +24,20 @@ target_table_ddl_ch = '''
         ORDER BY id
         '''
 
-api_to_postgres = ApiToPostgresOperator(
-    task_id='from_api_to_postgres',
-    api_url="https://random-data-api.com/api/cannabis/random_cannabis?size=10",
-    db_url='postgresql://username:password@localhost:5432/mydatabase',
-    target_table_name='cannabis_api_summary',
-    dag=dag)
+with DAG(dag_id, schedule_interval='0 */12 * * *', start_date=days_ago(2), catchup=False) as dag:
+    api_to_postgres = ApiToPostgresOperator(
+        task_id='from_api_to_postgres',
+        api_url="https://random-data-api.com/api/cannabis/random_cannabis?size=10",
+        db_url='postgresql://username:password@localhost:5432/mydatabase',
+        target_table_name='cannabis_api_summary',
+        dag=dag)
 
-api_to_clickhouse = ApiToClickhouseOperator(
-    task_id='from_api_to_clickhouse',
-    api_url="https://random-data-api.com/api/cannabis/random_cannabis?size=10",
-    db_url='clickhouse://test:test@localhost:56051/test',
-    target_table_name='cannabis_api_summary',
-    ddl=target_table_ddl_ch,
-    dag=dag)
+    api_to_clickhouse = ApiToClickhouseOperator(
+        task_id='from_api_to_clickhouse',
+        api_url="https://random-data-api.com/api/cannabis/random_cannabis?size=10",
+        db_url='clickhouse://test:test@localhost:56051/test',
+        target_table_name='cannabis_api_summary',
+        ddl=target_table_ddl_ch,
+        dag=dag)
 
-api_to_postgres
-api_to_clickhouse
+    api_to_postgres >> api_to_clickhouse
